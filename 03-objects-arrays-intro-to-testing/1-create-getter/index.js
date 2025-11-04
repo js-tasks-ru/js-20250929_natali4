6c0fbe1688d7cfa417374f529e09e2d3e82abf4e
natali <NataliDegtyreva@yandex.ru>
2025-11-04 14:50:04 +0300

    feat: solution for 03 - 1 (getter)

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,30 @@
+const getValueByKey = (obj, key) => {
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (!obj.hasOwnProperty(key)) {
+    return;
+  }
+
+  return obj[key];
+};
+
 /**
  * createGetter - creates function getter which allows select value from object
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function(obj) {
+    const keys = path.split('.');
 
+    let value = {...obj};
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      value = getValueByKey(value, key);
+    }
+    return value;
+  };
 }
+
