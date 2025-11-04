7b1597164ba2551fdd5adfc05c75006f7d4208b1
natali <NataliDegtyreva@yandex.ru>
2025-11-04 15:15:58 +0300

    feat: solution for 03 - 2 (invertObj)

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
+  let invertedObj = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    invertedObj[value] = key;
+  });
 
+  return invertedObj;
 }
